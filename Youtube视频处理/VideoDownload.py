ccb7d95c09f2c08fb6b0ab19453000a482c707e8
WoodsonWang <706469678@qq.com>
2021-03-21 16:55:10 +0800

    update

---
+++ Youtube视频处理/VideoDownload.py
@@ -0,0 +1,45 @@
+"""
+@author:Wang Xinsheng
+@File:VideoDownload.py
+@description:...
+@time:2021-03-21 13:22
+"""
+from constant import *
+from you_get import common as you_get
+import os
+from moviepy.editor import *
+
+def download(url):
+    out_dir = r'./Videos'
+    you_get.any_download(url=url,output_dir=out_dir,merge=True,itag=160)
+
+
+
+
+def get_info(url):
+    '''
+    显示资源信息
+    :param utl:
+    :return:
+    '''
+    os.system('you-get -i {}'.format(url))
+
+
+def mp4Tomp3(vido_path,outdir):
+    '''
+    将视频转换为mp3
+    :param vido_path:
+    :param outdir:
+    :return:
+    '''
+    video = VideoFileClip(vido_path)
+    audio = video.audio
+    audio.write_audiofile(outdir)
+
+
+
+if __name__ == '__main__':
+    url = 'https://www.youtube.com/watch?v=8xZ7b-iLg5I'
+    # get_info(url)
+    # download(url)
+    mp4Tomp3('./Videos/木吉他.mp4',outdir='./Videos/木吉他.mp3'.format(out_dir))
---
+++ Youtube视频处理/constant.py
@@ -0,0 +1,7 @@
+"""
+@author:Wang Xinsheng
+@File:constant.py
+@description:...
+@time:2021-03-21 16:14
+"""
+out_dir = './Audio'
